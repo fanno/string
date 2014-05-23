a916d1878897bd687fe8785b356ce3e32489d3a7
Michael Babker <mbabker@flbab.com>
2014-05-23 14:37:12 -0500

    Fix tests

--- Tests/StringTest.php
+++ Tests/StringTest.php
@@ -309,9 +309,9 @@ class StringTest extends \PHPUnit_Framework_TestCase
 	public function seedTestSubstr_replace()
 	{
 		return array(
-			array('321 Main Street', 'Broadway Avenue', 4, false, '321 Broadway Avenue'),
+			array('321 Main Street', 'Broadway Avenue', 4, null, '321 Broadway Avenue'),
 			array('321 Main Street', 'Broadway', 4, 4, '321 Broadway Street'),
-			array('чадна Би шил идэй чадна', '我能吞', 6, false, 'чадна 我能吞'),
+			array('чадна Би шил идэй чадна', '我能吞', 6, null, 'чадна 我能吞'),
 			array('чадна Би шил идэй чадна', '我能吞', 6, 2, 'чадна 我能吞 шил идэй чадна')
 		);
 	}
